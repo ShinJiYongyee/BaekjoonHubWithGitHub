86c78b1a48c9a9454a89145b5f110f64c3f56307
ShinJiYongyee <132910075+ShinJiYongyee@users.noreply.github.com>
2023-11-29 23:38:40 +0900

    [Bronze V] Title: 꼬마 정민, Time: 208 ms, Memory: 17576 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        long a,b,c;
+        a=input.nextLong();
+        b=input.nextLong();
+        c=input.nextLong();
+
+        System.out.println(a+b+c);
+    }
+
+}
