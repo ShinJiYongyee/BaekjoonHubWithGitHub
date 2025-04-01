511708e06587c3c17ffcff7e90a821175c7d3097
Shin Ji-yong(신지용) <132910075+ShinJiYongyee@users.noreply.github.com>
2025-04-01 14:28:54 +0900

    [level 0] Title: 피자 나눠 먹기 (2), Time: 0.22 ms, Memory: 31.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.cs
@@ -0,0 +1,16 @@
+using System;
+
+public class Solution {
+    public int solution(int n) {
+            int answer = 1;
+            while (true)
+            {
+                if (answer * 6 % n == 0)
+                {
+                    return answer;
+                }
+                answer++;
+            }
+            return answer;
+    }
+}
